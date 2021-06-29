343312b953e429d608cd0083f5b7481ee9f8135f
root-344 <michimasa8@gmail.com>
2021-06-29 17:45:58 +0900

    modal追加

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,2 +1,11 @@
 $(function() {
+  $('#cnt-name1').click(function() {
+    $('#modal1').fadeIn();
+  });
+  $('#cnt-name2').click(function() {
+    $('#modal2').fadeIn();
+  });
+  $('.close-modal').click(function() {
+    $('.modal-wrapper').fadeOut();
+  });
 });
