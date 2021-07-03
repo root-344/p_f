00501cce30dd4d8f2d42af5f3d1a4857496d85fa
root-344 <michimasa8@gmail.com>
2021-07-03 17:03:00 +0900

    scrollTop, animateメソッド導入

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -8,4 +8,16 @@ $(function() {
   $('.close-modal').click(function() {
     $('.modal-wrapper').fadeOut();
   });
+  $('#button1').click(function() {
+    var content1 = $('#content1').offset().top
+    $("html,body").animate({scrollTop:content1},300);
+  });
+  $('#button2').click(function() {
+    var content2 = $('#content2').offset().top
+    $("html,body").animate({scrollTop:content2},300);
+  });
+  $('#button3').click(function() {
+    var content3 = $('#content3').offset().top
+    $("html,body").animate({scrollTop:content3},300);
+  });
 });
